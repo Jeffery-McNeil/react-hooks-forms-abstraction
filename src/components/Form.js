41a043aa692437387e0cb3bcae67555abc273cb9
Jeffery <jeffmc825@gmail.com>
2021-12-15 23:46:29 -0600

    commit tree 38fc2875a4c03de28f610084371ef7da22e5ab55

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,42 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+  const [formData, setFormData] = useState({
+    firstName: "John",
+    lastName: "Henry",
+  })
 
   function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
+    setFormData({
+      ...formData,
+      firstName: event.target.value,
+    });
   }
 
   function handleLastNameChange(event) {
-    setLastName(event.target.value);
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
   }
 
   return (
     <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
+      <input 
+        type="text"
+        name="firstName" 
+        onChange={handleFirstNameChange} 
+        value={formData.firstName} 
+      />
+      <input
+        type="text"
+        name="lastName" 
+        onChange={handleLastNameChange} 
+        value={formData.lastName} 
+      />
       <button type="submit">Submit</button>
     </form>
   );
